8344dc18bb0d3a89a18d1f388b88ceac3342c424
meherarbi <58122305+meherarbi@users.noreply.github.com>
2021-02-24 22:37:48 +0100

    site

---
+++ src/Classe/Search.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Classe;
+use App\Entity\category;
+class Search
+{
+    /**
+     * @var string
+     */
+    public $string='';
+
+    /**
+     *@var category[]
+     */
+    public $categories=[];
+}
---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use ContainerLdBS6gX\getChangePasswordTypeService;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('old_password',PasswordType::class,['label'=>'Current Password','mapped'=>false])
+            ->add('new_password', RepeatedType::class,[
+                'mapped'=>false,
+                'type'=>PasswordType::class,
+                'invalid_message'=>'Invalid Password',
+                'required'=>true,
+                'first_options'=>['label'=>'New Password'],
+                'second_options'=>['label'=>'Confirm Password']
+            ])
+            ->add('submit',SubmitType::class,['label'=>'Update'])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
