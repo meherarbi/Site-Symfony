368d7845467a8402750d05c932b57f79fb5e6adb
Meher Arby <meher.arbi@gmail.com>
2023-04-23 22:56:22 +0100

    add categ & upload img

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -29,6 +29,11 @@ class Category
      */
     private $products;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
     public function __construct()
     {
         $this->products = new ArrayCollection();
@@ -85,6 +90,18 @@ class Category
                 $product->setCategory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
