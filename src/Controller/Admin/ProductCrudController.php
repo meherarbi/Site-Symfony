951633bbf57202cda488b01f1c87fb38980bc84c
Meher Arby <meher.arbi@gmail.com>
2023-04-24 20:03:38 +0100

    config deal

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -3,10 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
-use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
-use App\EventSubscriber\ImageUploadSubscriber;
-use App\Field\ImageUploadField;
-use App\Form\ImageUploadType;
 use App\Service\ElasticsearchService;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -18,12 +14,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
-use Vich\UploaderBundle\Form\Type\VichFileType;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-
+use EasyCorp\Bundle\EasyAdminBundle\Form\Util\FormTab;
 use Psr\Log\LoggerInterface;
 
 class ProductCrudController extends AbstractCrudController
@@ -32,7 +23,7 @@ class ProductCrudController extends AbstractCrudController
     private $entityManager;
     private $logger;
 
-    public function __construct(ElasticsearchService $elasticsearchService, EntityManagerInterface $entityManager , LoggerInterface $logger)
+    public function __construct(ElasticsearchService $elasticsearchService, EntityManagerInterface $entityManager, LoggerInterface $logger)
     {
         $this->elasticsearchService = $elasticsearchService;
         $this->entityManager = $entityManager;
@@ -44,78 +35,44 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
+
     public function configureFields(string $pageName): iterable
-    {
-
-        return [
-            FormField::addPanel('Détails du produit')->setIcon('fas fa-info-circle'),
-            TextField::new ('name')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            SlugField::new ('slug')->setTargetFieldName('name')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            TextField::new ('subtitle')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            TextareaField::new ('description')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            AssociationField::new ('category')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-
+{
+    return [
+        FormField::addPanel('Détails du produit')->setIcon('fas fa-info-circle'),
+            TextField::new('name')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+            SlugField::new('slug')->setTargetFieldName('name')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+            TextField::new('subtitle')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+            TextareaField::new('description')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+            AssociationField::new('category')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
             FormField::addPanel('Prix et promotion')->setIcon('fas fa-dollar-sign'),
-            MoneyField::new ('price')->setCurrency('EUR')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            MoneyField::new ('oldprice')->setCurrency('EUR')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-            BooleanField::new ('onPromotion', 'En promotion')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
-
-            FormField::addPanel('Images')->setIcon('fas fa-image'),
-            ImageField::new('illustration')
+        MoneyField::new('price')->setCurrency('EUR')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+        MoneyField::new('oldprice')->setCurrency('EUR')->setFormTypeOptions(['attr' => ['class' => 'col-md-6']]),
+        BooleanField::new('onPromotion', 'En promotion')->setFormTypeOptions(['attr' => ['class' => 'col-md-4']]),
+        BooleanField::new('isDeal', 'Deal of the week')->setFormTypeOptions(['attr' => ['class' => 'col-md-4']]),
+        BooleanField::new('isOnSale', 'Sale Products')->setFormTypeOptions(['attr' => ['class' => 'col-md-4']]),
+        FormField::addPanel('Images')->setIcon('fas fa-image'),
+        ImageField::new('illustration')
+                    ->setBasePath('uploads/')
+                    ->setUploadDir('public/uploads/')
+                    ->setUploadedFileNamePattern('[randomhash].[extension]')
+                    ->setRequired(false),
+        ImageField::new('imageFile', 'Images 1')
+                    ->setBasePath('uploads/')
+                    ->setUploadDir('public/uploads/')
+                    ->setUploadedFileNamePattern('[randomhash].[extension]')
+                    ->setRequired(false),
+        ImageField::new('img', 'Images 2')
+                    ->setBasePath('uploads/')
+                    ->setUploadDir('public/uploads/')
+                    ->setUploadedFileNamePattern('[randomhash].[extension]')
+                    ->setRequired(false),
+        ImageField::new('descriptionImage', 'Images de description')
                 ->setBasePath('uploads/')
                 ->setUploadDir('public/uploads/')
                 ->setUploadedFileNamePattern('[randomhash].[extension]')
-                ->setRequired( false),
-            ImageField::new('imageFile', 'Images 1')
-                ->setBasePath('uploads/')
-                ->setUploadDir('public/uploads/')
-                ->setUploadedFileNamePattern('[randomhash].[extension]')
-                ->setRequired(false),
-            ImageField::new('img', 'Images 2')
-                ->setBasePath('uploads/')
-                ->setUploadDir('public/uploads/')
-                ->setUploadedFileNamePattern('[randomhash].[extension]')
-                ->setRequired(false), 
-            ImageField::new('descriptionImage', 'Images de description')
-                ->setBasePath('uploads/')
-                ->setUploadDir('public/uploads/')
-                ->setUploadedFileNamePattern('[randomhash].[extension]')
-                ->setRequired(false)       
-                
-            
-              
-     
-           /*  CollectionField::new('images', 'Images supplémentaires')
-            ->setEntryType(ImageUploadType::class)
-            ->setFormTypeOption('by_reference', false), */
-        ];
-    }
-
-
-
-    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        parent::persistEntity($entityManager, $entityInstance);
-
-        // Indexer le produit dans Elasticsearch
-        $productData = [
-            'name' => $entityInstance->getName(),
-            'description' => $entityInstance->getDescription(),
-            // Ajoutez ici d'autres champs pertinents pour votre produit
-        ];
-        $this->elasticsearchService->indexDocument('products', $entityInstance->getId(), $productData);
-    }
-
-    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
-    {
-        parent::updateEntity($entityManager, $entityInstance);
-
-        // Update the product in Elasticsearch
-        $productData = [
-            'name' => $entityInstance->getName(),
-            'description' => $entityInstance->getDescription(),
-            // Add other relevant fields for your product here
-        ];
-        $this->elasticsearchService->indexDocument('products', $entityInstance->getId(), $productData);
-    }
+                ->setRequired(false)
+    ];
 }
+
+}    
